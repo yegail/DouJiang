3bd8a1e051844e5f816d0f5b5f14ca82db1bb7ec
mac <mac@macmatoiMac.local>
2013-01-06 16:28:56 +0800

    Initial Commit

---
+++ DouJiang/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DouJiang
+//
+//  Created by mac  on 13-1-6.
+//  Copyright (c) 2013年 mac . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
